f226764237e8035761209a89b81ea8bb4fcf9edb
HIMANSHU VERMA <hsverma2112@gmail.com>
2026-07-29 00:32:13 +0530

    Time: 7 ms (15.76%), Space: 46.9 MB (5.21%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,43 +1,83 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> st = new Stack<>();
-        String res = "/";
-        int n = path.length();
+        // Stack<String> st = new Stack<>();
+        // String res = "/";
+        // int n = path.length();
 
-        for(int i=0; i<n; i++){
-            while(i<n && path.charAt(i) == '/'){
-                i++;
-            }
+        // for(int i=0; i<n; i++){
+        //     while(i<n && path.charAt(i) == '/'){
+        //         i++;
+        //     }
             
-            String dir = "";
-            while(i<n && path.charAt(i) != '/'){
-                dir += path.charAt(i);
-                i++;
-            }
+        //     String dir = "";
+        //     while(i<n && path.charAt(i) != '/'){
+        //         dir += path.charAt(i);
+        //         i++;
+        //     }
 
-            if(dir.equals("..")){
-                if(!st.isEmpty()) st.pop(); 
-            }
-            else if(dir.equals(".") || dir.length() == 0){
+        //     if(dir.equals("..")){
+        //         if(!st.isEmpty()) st.pop(); 
+        //     }
+        //     else if(dir.equals(".") || dir.length() == 0){
+        //         continue;
+        //     }
+        //     else{
+        //         st.push(dir);
+        //     }
+        // }
+
+        // Stack<String> st2 = new Stack<>();
+        // while(!st.isEmpty()){
+        //     st2.push(st.pop());
+        // }
+
+        // while(!st2.isEmpty()){ 
+        //     if(st2.size() != 1){
+        //         res += (st2.pop() + "/");
+        //     }else{
+        //         res += st2.pop();
+        //     }
+        // }
+
+        // return res;
+        
+        Stack<String> st = new Stack<>();
+        String str1[] = path.split("/");
+        String res = "/";
+
+        for(String s : str1){
+            if("..".equals(s)){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }else if(".".equals(s) || "".equals(s)){
                 continue;
-            }
-            else{
-                st.push(dir);
+            }else{
+                st.push(s);
             }
         }
 
         Stack<String> st2 = new Stack<>();
+
+        int i=0;
         while(!st.isEmpty()){
-            st2.push(st.pop());
+            str1[i++] = st.pop();
         }
 
-        while(!st2.isEmpty()){ 
-            if(st2.size() != 1){
-                res += (st2.pop() + "/");
+        for(int j=i-1; j>=0; j--){
+            if(j != 0){
+                res = res+str1[j] + "/";
             }else{
-                res += st2.pop();
+                res = res+str1[j];
             }
         }
+        // while(!st2.isEmpty()){
+        //     if(st2.size()>1){
+        //         res = res+st2.pop() + "/";
+        //     }else{
+        //         res = res+st2.pop();
+        //     }
+        // }
 
         return res;
     }
