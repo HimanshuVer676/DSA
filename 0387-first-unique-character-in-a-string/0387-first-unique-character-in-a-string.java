a2d7b40caffe18ed912dba309bed8a980e3d61df
HIMANSHU VERMA <hsverma2112@gmail.com>
2026-08-01 00:13:04 +0530

    Time: 5 ms (98.23%), Space: 46.9 MB (73.52%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,17 +1,31 @@
 class Solution {
     public int firstUniqChar(String s) {
+        // int freq[] = new int[26];
+        // Queue<Integer> q = new LinkedList<>();
+
+        // for(int i=0; i<s.length(); i++){
+        //     freq[s.charAt(i)-'a']++;
+        //     q.add(i);
+
+        //     while(!q.isEmpty() && freq[s.charAt(q.peek())-'a'] > 1){
+        //         q.remove();
+        //     }
+        // }
+
+        // return q.isEmpty() ? -1 : q.peek();
+
         int freq[] = new int[26];
-        Queue<Integer> q = new LinkedList<>();
+        for(char c: s.toCharArray()){
+            freq[c-'a']++;
+        }
 
         for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i)-'a']++;
-            q.add(i);
-
-            while(!q.isEmpty() && freq[s.charAt(q.peek())-'a'] > 1){
-                q.remove();
+            char ch = s.charAt(i);
+            if(freq[ch - 'a'] == 1){
+                return i;
             }
         }
 
-        return q.isEmpty() ? -1 : q.peek();
+        return -1;
     }
 }
