6b70a5e6b815afc4bf8c68770d316b34ab6874f8
HIMANSHU VERMA <hsverma2112@gmail.com>
2026-08-14 17:34:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (92.22%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int bal = 0;
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == 'R'){
+                bal--;
+            }else{
+                bal++;
+            }
+
+            if(bal == 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
